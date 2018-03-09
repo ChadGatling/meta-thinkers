e16ae27c2ed053f671aa6b3e2e89526eb3d0d99a
Elliott Wendel <ewendel88@gmail.com>
2018-03-08 19:45:14 -0600

    added app.use's

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// Serve up static assets
+app.use(express.static("client/build"));
+
 app.listen(PORT, () => {
     console.log(`🌎  ==> API Server now listening on ${PORT}!`);
 });
